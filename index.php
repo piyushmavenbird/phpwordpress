3892a671256b3cebf62c19599330983b941eb3c7
Piyush Nandaniya <piyush@mavenbird.in>
2023-11-06 18:58:00 +0530

    phpwordpress prctice code

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Index</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="stylephp.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+   
+</head>
+<body>
+    <div class="index-title"><h1>CRUD</h1></div>
+    <div class="index-redirect">
+        <div class="createphp"><a href="create.php">Create</a></div>  <div class="readphp"><a href="read.php">Read</a></div>
+        
+    </div>
+</body>
+</html>
